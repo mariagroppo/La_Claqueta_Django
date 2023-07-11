a704d4fe526eb3217843d0625974e09a1ff29deb
Mey Groppo <mariagroppo86@gmail.com>
2023-07-10 22:58:22 -0300

    correcion en settings

--- La_Claqueta - Django/movies/movies/settings.py
+++ La_Claqueta - Django/movies/movies/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a70oh6ro^%(3)rn=#pk*yw@&2%5^we0s4iskgi-b-5tby8&1-(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
